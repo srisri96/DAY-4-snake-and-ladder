3dd93cac20bb42dc3436c60988669544201fe54b
srisri96 <srikanthsitharth80@gmil.com>
2022-07-18 16:49:46 +0530

    position

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,3 @@
+﻿using SnakeAndLadder;
+position.pos1();
+
---
+++ SnakeAndLadder/SnakeAndLadder/position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class position
+    {
+        public static void pos1()
+        {
+            int position = 0;
+            Console.WriteLine($"The position of player is {position}");
+        }
+    }
+}
